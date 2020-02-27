3c13e3bd10126e4af6ed3cb660d6439a338e4491
Richard George <richard@phase.org>
2020-02-26 20:08:17 -0500

    Add netcheck to verify network connectivity

---
+++ netcheck.py
@@ -0,0 +1,66 @@
+import socket
+import random
+from sense_hat import SenseHat
+from time import sleep
+
+
+targets = [
+    ('54.76.20.121', 80),
+    ('8.8.8.8', 53),
+    ('8.8.4.4', 53),
+]
+
+statusOk = False
+red = (255, 0, 0)
+green = (0, 255, 0)
+_ = (0, 0, 0)
+sense = SenseHat()
+sense.set_rotation(180)
+gray = (80, 80, 80)
+
+
+def drawLogo(c):
+    logo = [
+        _, _, c, c, c, c, _, _,
+        _, c, _, _, _, _, c, _,
+        c, _, _, c, c, _, _, c,
+        _, _, c, _, _, c, _, _,
+        _, c, _, _, _, _, c, _,
+        _, _, _, c, c, _, _, _,
+        _, _, _, _, _, _, _, _,
+        _, _, _, c, c, _, _, _,
+    ]
+    sense.set_pixels(logo)
+
+
+def drawPending(c):
+    logo = [
+        _, _, c, c, c, _, _, _,
+        _, c, _, _, _, c, _, _,
+        _, c, _, _, _, c, _, _,
+        _, _, _, _, c, _, _, _,
+        _, _, _, c, _, _, _, _,
+        _, _, _, c, _, _, _, _,
+        _, _, _, _, _, _, _, _,
+        _, _, _, c, _, _, _, _,
+    ]
+    sense.set_pixels(logo)
+
+
+displayColor = gray
+while True:
+    target = random.choice(targets)
+    try:
+        sense.clear()
+        drawPending(displayColor)
+        s = socket.create_connection(target, 10)
+        statusOk = True
+        s.close()
+    except Exception as e:
+        statusOk = False
+
+    displayColor = green if statusOk else red
+    drawLogo(displayColor)
+    print(target[0], end=" ")
+    print('.' if statusOk else 'x')
+    sleep(60)
